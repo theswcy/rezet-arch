629a77d6b36a67ce88519ec0e779b3ea8e8eeb5c
theswcy <theswcy@gmail.com>
2025-01-09 20:14:07 -0300

    Handler de eventos criado.

---
+++ bot/Handler-Events.cs
@@ -0,0 +1,14 @@
+using DSharpPlus;
+using DSharpPlus.EventArgs;
+
+
+
+
+namespace Rezet.Handlers {
+    public static class EventsHandler {
+        public static async Task SetupEvents(DiscordShardedClient client) {
+            // client.ComponentInteractionCreated
+            await Task.CompletedTask;
+        }
+    }
+}
