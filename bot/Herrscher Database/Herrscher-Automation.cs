151755414ba26aac4c59f7e4330b87797d858351
theswcy <theswcy@gmail.com>
2025-02-26 01:11:43 -0300

    update Herrscher-Automation.cs

---
+++ bot/Herrscher Database/Herrscher-Automation.cs
@@ -0,0 +1,5 @@
+// Automação de peças do banco de dados herrscher.
+
+// Itens:
+// - Coletar dados após início do programa e adicionar ao cache Luminy.
+// - Remover e adicionar dados ao cache Luminy em caso de ausência.
