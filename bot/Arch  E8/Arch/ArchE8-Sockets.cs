80c6b29d5154b4848ae482d1e9b2a55885963a1e
theswcy <theswcy@gmail.com>
2025-02-26 01:11:37 -0300

    update ArchE8-Sockets.cs

---
+++ bot/Arch  E8/Arch/ArchE8-Sockets.cs
@@ -0,0 +1,12 @@
+// Informações, status e recconect automático dos sockets.
+
+// Logs:
+// - Iniciada conexão com o socket.
+// - Fechada conexão com o socket.
+// = Erro com a conexão do socket.
+// - Detecção de conexão zumbi.
+
+// Automáticos:
+// - Reconnect após falha com o socket.
+// - Reconnect após conexão zumbi.
+// - Disconnect and Reconnect após falha grave.
