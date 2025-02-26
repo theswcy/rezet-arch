019196cfa1143e307ddc194b9fedcf78add4b8ed
theswcy <theswcy@gmail.com>
2025-02-26 01:11:38 -0300

    update ArchE8-Status.cs

---
+++ bot/Arch  E8/Arch/ArchE8-Status.cs
@@ -0,0 +1,12 @@
+// Configurações sobre o status do client.
+
+// Status com detecção automática:
+// - Online: ping abaixo de 100ms.
+// - Idle: ping acima de 100ms, apresentando falhas leves em serviços automáticos.
+// - Do not disturb: Apresentando falhas graves em todos os serviços.
+// - Invisible: offline, atualizando ou em manutenção.
+
+
+//                   Playing action! [ 0 - 50 / 20ms ]
+//                      Shard number <-|    |    |-> Ping da shard atual
+//        Quantidade de servers na shard <- |
