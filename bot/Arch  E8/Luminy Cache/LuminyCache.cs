85160a3a9be4352d5d6af17f7dc9e5f292b9a461
theswcy <theswcy@gmail.com>
2025-03-01 19:27:55 -0300

    update LuminyCache.cs

--- bot/Arch  E8/Luminy Cache/LuminyCache.cs
+++ bot/Arch  E8/Luminy Cache/LuminyCache.cs
@@ -2,6 +2,7 @@ using System;
 using System.Runtime.Caching;
 using MongoDB.Driver;
 using MongoDB.Bson;
+using DSharpPlus;
 
 
 
@@ -11,34 +12,32 @@ namespace Rezet.LuminyCache {
 
 
         // ========== SAVE THE GUILD:
-        public static void SaveGuild(string GuildId, BsonDocument Structure) {
+        public static void SaveGuild(DiscordClient client, string GuildId, BsonDocument Structure) {
             var policy = new CacheItemPolicy {
                 AbsoluteExpiration = DateTimeOffset.UtcNow.AddHours(24)
             };
 
 
-            int SelectedGuildShard = (int)((ulong.Parse(GuildId) >> 22) % (ulong)ArchE8.RezetRazor.ShardClients.Count);
-            if (SelectedGuildShard == 0) {
+            if (client.ShardId == 0) {
                 var LumiCache = new GuildCache_shard_0();
                 LumiCache.CacheShard0.Add(GuildId, Structure, policy);
-            } else if (SelectedGuildShard == 1) {
+            } else if (client.ShardId == 1) {
                 var LumiCache = new GuildCache_shard_1();
                 LumiCache.CacheShard1.Add(GuildId, Structure, policy);
-            } else if (SelectedGuildShard == 2) {
+            } else if (client.ShardId == 2) {
                 var LumiCache = new GuildCache_shard_2();
                 LumiCache.CacheShard2.Add(GuildId, Structure, policy);
             }
         }
         // ========== GET THE GUILD:
-        public BsonDocument? GetGuild(string GuildId) {
-            int SelectedGuildShard = (int)((ulong.Parse(GuildId) >> 22) % (ulong)ArchE8.RezetRazor.ShardClients.Count);
-            if (SelectedGuildShard == 0) {
+        public BsonDocument? GetGuild(DiscordClient client, string GuildId) {
+            if (client.ShardId == 0) {
                 var LumiCache = new GuildCache_shard_0();
                 return LumiCache.CacheShard0.Get(GuildId) as BsonDocument;
-            } else if (SelectedGuildShard == 1) {
+            } else if (client.ShardId == 1) {
                 var LumiCache = new GuildCache_shard_1();
                 return LumiCache.CacheShard1.Get(GuildId) as BsonDocument;
-            } else if (SelectedGuildShard == 2) {
+            } else if (client.ShardId == 2) {
                 var LumiCache = new GuildCache_shard_2();
                 return LumiCache.CacheShard2.Get(GuildId) as BsonDocument;
             } else {
@@ -46,15 +45,14 @@ namespace Rezet.LuminyCache {
             }
         }
         // ========== REMOVE THE GUILD:
-        public static void RemoveGuild(string GuildId) {
-            int SelectedGuildShard = (int)((ulong.Parse(GuildId) >> 22) % (ulong)ArchE8.RezetRazor.ShardClients.Count);
-            if (SelectedGuildShard == 0) {
+        public static void RemoveGuild(DiscordClient client, string GuildId) {
+            if (client.ShardId == 0) {
                 var LumiCache = new GuildCache_shard_0();
                 LumiCache.CacheShard0.Remove(GuildId);
-            } else if (SelectedGuildShard == 1) {
+            } else if (client.ShardId == 1) {
                 var LumiCache = new GuildCache_shard_1();
                 LumiCache.CacheShard1.Remove(GuildId);
-            } else if (SelectedGuildShard == 2) {
+            } else if (client.ShardId == 2) {
                 var LumiCache = new GuildCache_shard_2();
                 LumiCache.CacheShard2.Remove(GuildId);
             }
