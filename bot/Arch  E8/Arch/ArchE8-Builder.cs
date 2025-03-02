70ed7c07c5dc3967c58e6c5512d08efbf987137f
theswcy <theswcy@gmail.com>
2025-03-02 00:38:53 -0300

    Handlers e Socket logs adicionados.

--- bot/Arch  E8/Arch/ArchE8-Builder.cs
+++ bot/Arch  E8/Arch/ArchE8-Builder.cs
@@ -1,54 +1,71 @@
 using System.Text.Json;
 using DSharpPlus;
-using DSharpPlus.Entities;
-using DSharpPlus.Interactivity.Extensions;
-using DSharpPlus.CommandsNext;
+using Rezet.HerrscherSearch;
+using Rezet.Logging;
 
 
 
 
+#pragma warning disable CS8604
 namespace Rezet {
     // ========== REZET ARCH ENGINE 8.
     class ArchE8 {
         // ========== IGNITES:
         public static DiscordShardedClient? RezetRazor;
-        // public static HerrscherService? HerrscherRazor;
+        public static HerrscherServices? HerrscherRazor;
 
 
 
         public static async Task EngineStart() {
             // ========== GET THE TOKEN:
-            var configPath = Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "obj", "config.json");
-            var configJson = await File.ReadAllTextAsync(configPath);
-            var config = JsonSerializer.Deserialize<Config>(configJson);
+            try {
+                RezetLogs.EngineStart("Arch 1.0.0");
+                var configPath = Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "obj", "config.json");
+                var configJson = await File.ReadAllTextAsync(configPath);
+                var config = JsonSerializer.Deserialize<Config>(configJson);
 
 
 
 
-            // ========== CLIENT BUILDER:
-            var rzt = new DiscordConfiguration {
-                Token = config?.Token,
-                TokenType = TokenType.Bot,
-                ShardCount = 1,
-                Intents = 
-                    DiscordIntents.Guilds |
-                    DiscordIntents.GuildBans | 
-                    DiscordIntents.GuildIntegrations |
-                    DiscordIntents.GuildMembers |
-                    DiscordIntents.GuildMessages |
-                    DiscordIntents.GuildMessageTyping |
-                    DiscordIntents.GuildPresences |
-                    DiscordIntents.MessageContents,
-                ReconnectIndefinitely = true,
-                GatewayCompressionLevel = GatewayCompressionLevel.None,
-                MinimumLogLevel = Microsoft.Extensions.Logging.LogLevel.Error,
-            };
-            RezetRazor = new DiscordShardedClient(rzt);
-            
-            await RezetRazor.StartAsync();
-            await Handlers.CommandsHandler.SetupSlashCommands(RezetRazor);
-            await Handlers.EventsHandler.SetupEvents(RezetRazor);
-            await Task.Delay(-1);
+                // ========== CLIENT BUILDER:
+                var rzt = new DiscordConfiguration {
+                    Token = config?.Token,
+                    TokenType = TokenType.Bot,
+                    ShardCount = 1, // NOT FINISHED!
+                    Intents = 
+                        DiscordIntents.Guilds |
+                        DiscordIntents.GuildBans | 
+                        DiscordIntents.GuildIntegrations |
+                        DiscordIntents.GuildMembers |
+                        DiscordIntents.GuildMessages |
+                        DiscordIntents.GuildMessageTyping |
+                        DiscordIntents.GuildPresences |
+                        DiscordIntents.MessageContents,
+                    ReconnectIndefinitely = true,
+                    GatewayCompressionLevel = GatewayCompressionLevel.None,
+                    MinimumLogLevel = Microsoft.Extensions.Logging.LogLevel.Error,
+                };
+                RezetRazor = new DiscordShardedClient(rzt);
+
+
+
+                // ========== HERRSCHER CONNECT;
+                HerrscherRazor = new HerrscherServices(config?.Keydb, "extension");
+
+
+
+                // ========== START!
+                await RezetRazor.StartAsync();
+                await Handlers.CommandsHandler.SetupSlashCommands(RezetRazor);
+                await Handlers.CommandsHandler.SetupPrefixCommands(RezetRazor);
+                await Handlers.EventsHandler.SetupEvents(RezetRazor);
+                await Status.DiscordStatus.StatusReady(RezetRazor);
+                await AOCore.ShardsSockets.SetupSocketsEvents(RezetRazor);
+                RezetLogs.FinishedBuild();
+                await Task.Delay(-1);
+            } catch (Exception ex) {
+                RezetLogs.ErrorBuild(ex.Message);
+            }
         }
     }
     class Config { public string? Token { get; set; } public string? Keydb { get; set; } }
