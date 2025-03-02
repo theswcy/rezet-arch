26013155aacdf6139ff0a1d64bddb7a38a019ba9
theswcy <theswcy@gmail.com>
2025-03-02 00:40:06 -0300

    Configurações de status adicionado.

--- bot/Arch  E8/Arch/ArchE8-Status.cs
+++ bot/Arch  E8/Arch/ArchE8-Status.cs
@@ -10,3 +10,40 @@
 //                   Playing action! [ 0 - 50 / 20ms ]
 //                      Shard number <-|    |    |-> Ping da shard atual
 //        Quantidade de servers na shard <- |
+
+
+
+
+using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.EventArgs;
+
+
+
+namespace Rezet.Status {
+    public class DiscordStatus {
+        public static readonly TimeSpan StatusUpdateInterval = TimeSpan.FromSeconds(20);
+        public static async Task StatusReady(DiscordShardedClient client) {
+            foreach (var shard in client.ShardClients.Values) {
+                shard.Ready += UpdateStatus;
+            }
+            await Task.CompletedTask;
+        }
+
+
+
+        private static async Task UpdateStatus(DiscordClient client, ReadyEventArgs e) {
+            while (true) {
+                var activity = new DiscordActivity($"action! [ {client.ShardId} - {client.Guilds.Count} / {client.Ping}ms ]", ActivityType.Playing);
+                if (client.Ping > 200) {
+                    await client.UpdateStatusAsync(activity, UserStatus.DoNotDisturb);
+                } else if (client.Ping > 100) {
+                    await client.UpdateStatusAsync(activity, UserStatus.Idle);
+                } else {
+                    await client.UpdateStatusAsync(activity, UserStatus.Online);
+                }
+                await Task.Delay(StatusUpdateInterval);
+            }
+        }
+    }
+}
