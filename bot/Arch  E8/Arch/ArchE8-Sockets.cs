74cb02eb6a3ebb6627eb53402fb640441d8ea4fd
theswcy <theswcy@gmail.com>
2025-03-02 00:39:20 -0300

    Logging dos sockets adicionados.

--- bot/Arch  E8/Arch/ArchE8-Sockets.cs
+++ bot/Arch  E8/Arch/ArchE8-Sockets.cs
@@ -1,26 +1,46 @@
-// Informações, status e recconect automático dos sockets.
-
-// Logs:
-// - Iniciada conexão com o socket.
-// - Fechada conexão com o socket.
-// = Erro com a conexão do socket.
-// - Detecção de conexão zumbi.
-
-// Automáticos:
-// - Reconnect após falha com o socket.
-// - Reconnect após conexão zumbi.
-// - Disconnect and Reconnect após falha grave.
-
-
-
 using DSharpPlus;
-using DSharpPlus.Entities;
 using DSharpPlus.EventArgs;
+using Rezet.Logging;
 
 
 
-namespace Rezet.Sockets {
-    public class DiscordSockets {
-        
+namespace Rezet.AOCore {
+    public class ShardsSockets {
+        public static async Task SetupSocketsEvents(DiscordShardedClient client) {
+            foreach(var shard in client.ShardClients.Values) {
+                shard.SocketOpened += OnSocketOpened;
+                shard.SocketClosed += OnSocketClosed;
+                shard.SocketErrored += OnSocketErrored;
+            }
+            await Task.CompletedTask;
+        }
+
+
+        public static async Task OnSocketOpened(DiscordClient client, EventArgs e) {
+            RezetLogs.SocketOpened(client.ShardId);
+            await Task.CompletedTask;
+        }
+
+
+        public static async Task OnSocketClosed(DiscordClient client, SocketCloseEventArgs e) {
+            int retryCount = 0;
+            while (retryCount < 15) {
+                RezetLogs.SocketClosed(e.CloseMessage, client.ShardId);
+                try {
+                    await client.ReconnectAsync();
+                } catch (Exception ex) {
+                    RezetLogs.SocketClosedReconnect(ex.Message, client.ShardId);
+                    retryCount++;
+                    await Task.Delay(TimeSpan.FromSeconds(5));
+                }
+            }
+            RezetLogs.SocketClosedError(e.CloseMessage, client.ShardId);
+        }
+
+
+        public static async Task OnSocketErrored(DiscordClient client, SocketErrorEventArgs e) {
+            RezetLogs.SocketErrored(e.Exception.Message, client.ShardId);
+            await Task.CompletedTask;
+        }
     }
 }
