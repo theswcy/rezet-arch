5b87cf1f915c6dfe4610b007c28ab9eed1a1d10f
theswcy <theswcy@gmail.com>
2025-03-02 13:40:17 -0300

    Adicionado Herrscher Automation.

--- bot/Herrscher Database/Herrscher-Automation.cs
+++ bot/Herrscher Database/Herrscher-Automation.cs
@@ -1,5 +1,26 @@
-// Automação de peças do banco de dados herrscher.
+using MongoDB.Driver;
+using MongoDB.Bson;
+using DSharpPlus.Entities;
+using DSharpPlus;
 
-// Itens:
-// - Coletar dados após início do programa e adicionar ao cache Luminy.
-// - Remover e adicionar dados ao cache Luminy em caso de ausência.
+
+
+#pragma warning disable CS8602
+namespace Rezet.HerrscherAutomation {
+    public class HerrscherAutomation {
+        public static async Task UpdateAdd(DiscordClient client, DiscordGuild Guild, string map, dynamic content) {
+            var Herrscher = ArchE8.HerrscherRazor.GetHerrscherStructure(Guild);
+            var Collection = ArchE8.HerrscherRazor?._database?.GetCollection<BsonDocument>("guilds");
+            var update = Builders<BsonDocument>.Update.Set($"{Guild.Id}.{map}", content);
+            
+            await Collection.UpdateOneAsync(Herrscher, update);
+        }
+        public static async Task UpdateRemove(DiscordClient client, DiscordGuild Guild, string map) {
+            var Herrscher = ArchE8.HerrscherRazor.GetHerrscherStructure(Guild);
+            var Collection = ArchE8.HerrscherRazor?._database?.GetCollection<BsonDocument>("guilds");
+            var update = Builders<BsonDocument>.Update.Unset($"{Guild.Id}.{map}");
+
+            await Collection.UpdateOneAsync(Herrscher, update);
+        }
+    }
+}
