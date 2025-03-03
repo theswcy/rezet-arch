c9f9e67f71490058b497125ad50f1fc9a4e5ec25
theswcy <theswcy@gmail.com>
2025-03-02 23:03:03 -0300

    Adicionado verificação de permissões.

---
+++ bot/Verify/VerifyPermissions.cs
@@ -0,0 +1,66 @@
+using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+using DSharpPlus.CommandsNext;
+
+
+
+namespace Rezet.Verify {
+    public class VerifyPermissions {
+        public static async Task<bool> VerifyUserSlash(InteractionContext ctx, Permissions permission) {
+            var m = await ctx.Guild.GetMemberAsync(ctx.User.Id);
+            
+
+            if (!m.Permissions.HasPermission(permission)) {
+                await ctx.EditResponseAsync(
+                    new DiscordWebhookBuilder()
+                        .WithContent($"Hey, você não tem a permissão `{permission}` para usar o comando!")
+                );
+                return false;
+            } else {
+                return true;
+            }
+        }
+        public static async Task<bool> VerifyUserPrefix(CommandContext ctx, Permissions permission) {
+            var m = await ctx.Guild.GetMemberAsync(ctx.User.Id);
+
+
+            if (!m.Permissions.HasPermission(permission)) {
+                await ctx.RespondAsync($"Hey, você não tem a permissão `{permission}` para usar o comando!");
+                return false;
+            } else {
+                return true;
+            }
+        }
+
+
+
+
+
+        public static async Task<bool> VerifyRezetSlash(InteractionContext ctx, Permissions permission) {
+            var m = await ctx.Guild.GetMemberAsync(ctx.Client.CurrentUser.Id);
+            
+
+            if (!m.Permissions.HasPermission(permission)) {
+                await ctx.EditResponseAsync(
+                    new DiscordWebhookBuilder()
+                        .WithContent($"Hey, eu não tenho a permissão `{permission}` para executar o comando!")
+                );
+                return false;
+            } else {
+                return true;
+            }
+        }
+        public static async Task<bool> VerifyRezetPrefix(CommandContext ctx, Permissions permission) {
+            var m = await ctx.Guild.GetMemberAsync(ctx.Client.CurrentUser.Id);
+
+
+            if (!m.Permissions.HasPermission(permission)) {
+                await ctx.RespondAsync($"Hey, eu não tenho a permissão `{permission}` para executar o comando!");
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
+}
