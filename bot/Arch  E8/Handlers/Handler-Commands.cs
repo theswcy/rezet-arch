f542306ef739814f3d6c55f3269e5340cd14d596
theswcy <theswcy@gmail.com>
2025-03-06 16:32:10 -0300

    🔵 | Handler de comandos atualizado.

--- bot/Arch  E8/Handlers/Handler-Commands.cs
+++ bot/Arch  E8/Handlers/Handler-Commands.cs
@@ -7,28 +7,37 @@ using Rezet.Logging;
 
 
 namespace Rezet.Handlers {
-    public static class CommandsHandler {
-        public static async Task SetupPrefixCommands(DiscordShardedClient client) {
-            foreach (var shard in client.ShardClients.Values) {
-                var commands = shard.UseCommandsNext(new CommandsNextConfiguration {
-                    StringPrefixes = [ "r." ],
-                    CaseSensitive = false,
-                    EnableDms = false,
-                    EnableMentionPrefix = true
-                });
-            }
+    public class CommandsHandlers {
+        public static async Task Activate(DiscordShardedClient Rezet) {
+            await SyncPrefix(Rezet);
+            await SyncSlash(Rezet);
+        }
+        private static async Task SyncPrefix(DiscordShardedClient ShardedRezet) {
+            var prefix = ShardedRezet.UseCommandsNextAsync(new CommandsNextConfiguration {
+                StringPrefixes = [ "r." ],
+                CaseSensitive = false,
+                EnableDms = false,
+                EnableMentionPrefix = true
+            });
+
+
+            
             RezetLogs.PrefixCommandsConnect();
             await Task.CompletedTask;
         }
-        public static async Task SetupSlashCommands(DiscordShardedClient client) {
-            foreach (var shard in client.ShardClients.Values) {
-                var slash = shard.UseSlashCommands();
+        private static async Task SyncSlash(DiscordShardedClient ShardedRezet) {
+            var slash = await ShardedRezet.UseSlashCommandsAsync();
 
-                slash.RegisterCommands<PartnershipCommands>();
-                slash.RegisterCommands<CommunityCommands>();
-                slash.RegisterCommands<RolesCommands>();
-                slash.RegisterCommands<ChatCommands>();
-            }
+
+
+            slash.RegisterCommands<PartnershipCommands>();
+            slash.RegisterCommands<CommunityCommands>();
+            slash.RegisterCommands<RolesCommands>();
+            slash.RegisterCommands<ChatCommands>();
+            slash.RegisterCommands<RezetSystems>();
+
+
+
             RezetLogs.SlashCommandsConnect();
             await Task.CompletedTask;
         }
