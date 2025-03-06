0073be933ac1d2091a9b579705a8c1ecbbc41924
theswcy <theswcy@gmail.com>
2025-03-06 16:32:20 -0300

    🔵 | Handler de eventos atualizado.

--- bot/Arch  E8/Handlers/Handler-Events.cs
+++ bot/Arch  E8/Handlers/Handler-Events.cs
@@ -7,7 +7,10 @@ using Rezet.Logging;
 
 namespace Rezet.Handlers {
     public static class EventsHandler {
-        public static async Task SetupEvents(DiscordShardedClient client) {
+        public static async Task Activate(DiscordShardedClient Rezet) {
+            await SyncEvents(Rezet);
+        }
+        private static async Task SyncEvents(DiscordShardedClient client) {
             // client.ComponentInteractionCreated
             RezetLogs.EventsCommandsConnect();
             await Task.CompletedTask;
