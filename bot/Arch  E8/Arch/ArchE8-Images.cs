19facf16d45ae8fe0195f801c5079fff87d8ddde
theswcy <theswcy@gmail.com>
2025-03-14 14:29:43 -0300

    🟢 | Adicionado método para retornar imagens.

---
+++ bot/Arch  E8/Arch/ArchE8-Images.cs
@@ -0,0 +1,8 @@
+namespace Rezet.Images {
+    public class RezetImages {
+        public static string ArchDivinder() {
+            string img = "https://media.discordapp.net/attachments/1344162058996027453/1344169220526571520/IMG_20250226_014822.png";
+            return img;
+        }
+    }
+}
