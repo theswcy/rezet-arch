0ea7964ddc45a37d11e653c53c6f84535b6ab0e3
theswcy <theswcy@gmail.com>
2025-03-26 00:32:36 -0300

    HANDLER: Handler de eventos aprimorados e adicionado evento de testes,

--- bot/Arch  E8/Handlers/Handler-Events.cs
+++ bot/Arch  E8/Handlers/Handler-Events.cs
@@ -1,17 +1,33 @@
 using DSharpPlus;
 using DSharpPlus.EventArgs;
 using Rezet.Logging;
+using Rezet.Events.SystemEvents;
 
 
 
 
 namespace Rezet.Handlers {
     public static class EventsHandler {
-        public static async Task Activate(DiscordShardedClient Rezet) {
-            await SyncEvents(Rezet);
-        }
-        private static async Task SyncEvents(DiscordShardedClient client) {
-            // client.ComponentInteractionCreated
+        public static async Task SyncEvents(DiscordShardedClient client) {
+
+
+
+            // ========== GUILD BASIC ACTIONS.
+            client.GuildCreated += async (client, args) => {
+                await Task.Run(async () => {
+                    try {
+                        await AddGuildOnHerrscher.Update(client, args);
+                    } catch (Exception ex) {
+                        RezetLogs.HandlerOperation(
+                            "GUILD CREATE",
+                            $"- {ex.GetType()}\n- {ex.Message}\n{ex.StackTrace}"
+                        );
+                    }
+                });
+            };
+
+
+
             RezetLogs.EventsCommandsConnect();
             await Task.CompletedTask;
         }
